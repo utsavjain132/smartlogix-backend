1681e9133f3d1d1a11226b09bd045ea6ddf37a57
Aadesh Kumar <kaadesh18001@gmail.com>
2025-12-20 20:39:42 +0530

    [update] db.js file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const app = require("./src/app");
+const app = require("./src/app");
 
 app.listen(7858, () => {
     console.log("Server Started: http://localhost:7858")
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+const connectDB = require("./config/db")
 const app = express();
 
 app.use("express.json");
+connectDB()
 
 app.use("/api/auth", require("./routes/auth.routes"))
 
